d46ce04724485da9758579ad4890b1c5027abbb8
Hadi Al Zein <hadialzein123@gmail.com>
2023-07-13 23:43:49 +0200

    ✨Added testing functions for model evaluation

---
+++ src/test.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def load_model(path, custom_objects=None):
+    return tf.keras.models.load_model(path, custom_objects=custom_objects)
+
+def predict_depth(x, model):
+    predictions = model.predict(x)
+    return predictions
+
+def compute_test_loss(actual, predictions, loss="mae"):
+    if loss == "mae":
+        total_error = 0
+        total_pairs = 0
+
+        for pred_img, actual_img in zip(predictions, actual):
+            error = np.abs(pred_img - actual_img)
+            total_error += np.sum(error)
+            total_pairs += error.size
+
+        loss = total_error / total_pairs
+
+    else:
+        raise ValueError("Loss is not Implemented")
+
+    return loss
+
+def visualize_predictions(image, actual_depth, pred_depth):
+    fig, axs = plt.subplots(1, 3)
+
+    pred_depth = Image.fromarray(pred_depth)
+    actual_depth = Image.fromarray(actual_depth)
+
+    im1 = axs[0].imshow(image)
+    axs[0].axis('off')
+    axs[0].set_title("Original Image")
+
+    im2 = axs[1].imshow(actual_depth, vmin=0, vmax=1)
+    axs[1].axis('off')
+    axs[1].set_title("Actual Depth")
+    cbar2 = fig.colorbar(im2, ax=axs[1])
+
+    im3 = axs[2].imshow(pred_depth, vmin=0, vmax=1)
+    axs[2].axis('off')
+    axs[2].set_title("Actual Depth")
+    cbar3 = fig.colorbar(im3, ax=axs[2])
+
+    plt.subplots_adjust(wspace=0.3)
+
+    plt.show()
+
+
+
+
